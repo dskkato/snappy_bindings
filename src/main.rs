9226482db23a3d1c6d1e86aaf53ff74451c6be32
Daisuke Kato <kato.daisuke429@gmail.com>
2019-03-21 19:05:05 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 extern crate libc;
+use libc::size_t;
+
+#[link(name = "snappy")]
+extern "C" {
+    fn snappy_max_compressed_length(source_length: size_t) -> size_t;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = unsafe { snappy_max_compressed_length(100) };
+    println!("max compressed length of a 100 byte buffer: {}", x);
 }
